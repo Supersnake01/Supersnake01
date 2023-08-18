1b7225efacd967673fff63e05d05265f28de86af
Supersnake01 <139215783+Supersnake01@users.noreply.github.com>
2023-08-18 09:21:20 +0300

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,55 +1,5 @@
-import turtle
-import random
-a=set("абвгдежзийклмнопрстуфхцчшщыьэюяabcdefghijklmnopqrstuvwxyz")
-l=["hi","man"]
-w=random.choice(l)
-s=0
-o=set()
-x=len(w)
-print("enter word,lenght=",x)
-k="_"*x
-print(k)
-m=list(w)
-while s<11:
-    if set(w)==o:
-        print("you win")
-        print(w)
-        break
-    i=str(input("enter one letter of the word or full word:"))
-    if len(i)==1 or len(i)==x:
-        if set(i)==set(w):
-            print("you win")
-            print(w)
-            break
-        else:
-            if i in w:
-                print("true")
-                o.add(i)
-                new=""
+
                 
-                for h in range(len(w)):
-                    if i==w[h]:
-                        new+=i
-                    else:
-                        new+=k[h]
-                k=new
-                print(k)
-                        
-                if i in a:
-                    a=a-set(i)
-                else:
-                    print("you have used this letter")
-            else:
-                print("no")
-                s=s+1
-                if i in a:
-                    a=a-set(i)
-                if i in o:
-                    print("you have used this letter")
-        
-    else:
-        print("you can't")
-    if s >=11:
-        print("you have killed")
+            
 
             
